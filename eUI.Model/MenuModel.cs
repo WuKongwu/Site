00c7423f1da520ef6eaacd804c8c5dbbfea9ff31
WuKongwu <yhyjy8@126.com>
2017-01-03 16:42:20 +0800

    Menu

---
+++ eUI.Model/MenuModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUI.Model
+{
+    public class  MenuModel
+    {
+        public int Id { get; set; }
+        public string MenuName { get; set; }
+        public int  Type{ get; set; }
+        public string ChildImageURL { get; set; }
+        public int OrderIndex { get; set; }
+    }
+}
